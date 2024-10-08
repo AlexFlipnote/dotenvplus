cf6127ed1cb94a146a23359ffc39e712f412ad39
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2024-10-08 16:13:27 +0200

    Update example environment variables to uppercase and adjust example script accordingly

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -3,7 +3,8 @@ from dotenvplus import DotEnv
 env = DotEnv(".env.example")
 
 print(env)
-print(env["string_value"])
+print(env["STRING_VALUE"])
+print(env["VAR_VALUE"])
 
 for k, v in env.items():
     print(f"{k}: {v}")
